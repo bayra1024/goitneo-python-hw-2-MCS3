a2d350d941ba914073ce35455d1822aae58b6007
bayra1024 <bayra1024@gmail.com>
2023-10-23 03:24:49 +0300

    upd_q

--- telephones.py
+++ telephones.py
@@ -61,7 +61,7 @@ def main():
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
+        if command in ["close", "exit", "q"]:
             print("Good bye!")
             break
         elif command == "hello":
